0b08be8873e1d88fad6504dabdeada0f25cb4d46
Pierre-Yves Fourmond <pierre-yves.fourmond@octo.com>
2024-02-10 07:51:49 +0100

    Un Shell de lancement plus lisible

--- bin/Application.sh
+++ bin/Application.sh
@@ -8,9 +8,10 @@ get_absolute_path() {
     realpath "$path"
 }
 
-script_name=$(basename "$0")
-app_name=${script_name/sh/java}
+shell_script_name=$(basename "$0")
 bin_dir=$(get_absolute_path "$(dirname "$0")")
-app_dir=$(dirname "${bin_dir}")
 
-APP_DIR=${app_dir} "${app_dir}/src/${app_name}" "$@"
+app_dir=$(dirname "${bin_dir}")
+java_file_name=${shell_script_name/sh/java}
+
+APP_DIR=${app_dir} "${app_dir}/src/${java_file_name}" "$@"
