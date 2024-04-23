63a0c7b9abb4381fba2107865d735b07e28c107a
Pierre-Yves Fourmond <pierre-yves.fourmond@octo.com>
2024-04-23 09:07:39 +0200

    Variable mal nommée

--- src/Application.java
+++ src/Application.java
@@ -14,11 +14,11 @@ class Main {
 
         public static Language guess() {
             var langEnvVar = System.getenv("LANG");
-            var lang = langEnvVar.substring(0, langEnvVar.indexOf('_'));
-            return switch (lang) {
+            var langPrefix = langEnvVar.substring(0, langEnvVar.indexOf('_'));
+            return switch (langPrefix) {
                 case "fr" -> French;
                 case "en" -> English;
-                default -> throw new IllegalStateException(STR."Unexpected Language: \{lang}");
+                default -> throw new IllegalStateException(STR."Unexpected Language: \{langPrefix}");
             };
         }
     }
