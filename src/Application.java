08084aa814965e2ec11e2207489ffe5ab0d399b0
Pierre-Yves Fourmond <pierre-yves.fourmond@octo.com>
2024-04-25 08:38:34 +0200

    Renommage

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,13 @@
 ///usr/bin/env java --source 22 --enable-preview --class-path ${APP_DIR:-.}/lib/'*' "$0" "$@"; exit $?
 
-import static language.Language.*;
+import language.Language;
+import language.en.Hello;
+import language.fr.Bonjour;
 
 void main() {
-    switch (guess()) {
-        case French -> language.fr.Hello.main();
-        case English -> language.en.Hello.main();
+    switch (Language.guess()) {
+        case French -> Bonjour.main();
+        case English -> Hello.main();
     }
     System.exit(0);
 }
--- src/language/fr/Bonjour.java
+++ src/language/fr/Bonjour.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import net.fellbaum.jemoji.Emoji;
 import net.fellbaum.jemoji.EmojiManager;
 
-public class Hello {
+public class Bonjour {
     public static void main() {
         Optional<Emoji> optionalEmoji = EmojiManager.getByAlias("fr");
         String emoji = optionalEmoji.map(value -> STR." \{value.getEmoji()}").orElse("");
