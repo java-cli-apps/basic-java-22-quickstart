f34a7cec24cfdeeba074e6529ce970b489594cbb
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2025-05-31 05:47:48 +0200

    Ne pas positionner APP_DIR dans le Makefile

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,4 @@
-///usr/bin/env java --source 22 --enable-preview --class-path ${APP_DIR}/lib/'*' "$0" "$@"; exit $?
+///usr/bin/env java --source 22 --enable-preview --class-path ${APP_DIR:-.}/lib/'*' "$0" "$@"; exit $?
 
 import language.Language;
 import language.en.Hello;
